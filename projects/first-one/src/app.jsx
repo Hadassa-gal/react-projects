6c372c7dc7013e9f82f7134c2363aca98b30c17e
Hadassa-gal <hadassa.rgr@gmail.com>
2025-11-15 11:40:43 -0500

    .

---
+++ projects/first-one/src/app.jsx
@@ -0,0 +1,18 @@
+export function App(){
+    return (
+        <article style={ {display: 'flex', color: '#fff'}}>
+            <header>
+                <img src="https://i.pinimg.com/736x/47/fa/60/47fa605e1b85ac99bed524e5a3122110.jpg" alt="avatar1" srcset="" />
+                <div>
+                    <strong>yo</strong>
+                    <span>aaa</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
