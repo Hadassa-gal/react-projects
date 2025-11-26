866eb19addcdb24bbdd32d82d2ee76c29b12f96c
Hadassa-gal <hadassa.rgr@gmail.com>
2025-11-26 16:36:29 -0500

    feat: :zap: something

--- projects/first-one/src/app.jsx
+++ projects/first-one/src/app.jsx
@@ -1,15 +1,16 @@
-export function App(){
+import './app.css'
+export function App({name}, {appName}, {imgUrl}) {
     return (
-        <article style={ {display: 'flex', color: '#fff'}}>
-            <header>
-                <img src="https://i.pinimg.com/736x/47/fa/60/47fa605e1b85ac99bed524e5a3122110.jpg" alt="avatar1" srcset="" />
-                <div>
-                    <strong>yo</strong>
-                    <span>aaa</span>
+        <article className='article-card'>
+            <header className='head-card'>
+                <img className='img-card' src={imgUrl} alt="avatar1" srcset="" />
+                <div className='div-card'>
+                    <strong className='strong1-card'>{name}</strong>
+                    <span className='span1-card'>{appName}</span>
                 </div>
             </header>
-            <aside>
-                <button>
+            <aside className='aside1-card'>
+                <button className='buttonfollow-card'>
                     Seguir
                 </button>
             </aside>
--- projects/first-one/src/main.jsx
+++ projects/first-one/src/main.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {App} from './App.jsx'
+import {App} from './app'
+import './index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
-  <App />
+  <>
+    <App name={'hadassa'} appName={'something'} imgUrl={'https://i.pinimg.com/736x/47/fa/60/47fa605e1b85ac99bed524e5a3122110.jpg'} />
+    <App name={'haaaa'} appName={'something'} imgUrl={'https://i.pinimg.com/736x/47/fa/60/47fa605e1b85ac99bed524e5a3122110.jpg'} />
+  </>
 )
+// hghg
